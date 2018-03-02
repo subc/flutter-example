294dc7e84d6ca2ffc2f4bf986d40120044ab6f73
Shuhei Ikeda <shuhei.ikeda@o-09796-mac.local>
2018-03-02 19:53:13 +0900

    feat: send message button

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,27 @@ class ChatScreenState extends State<ChatScreen> {
     );
   }
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(
-            hintText: "メッセージ送信"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(hintText: "send a message"),
+              )
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ],
+        )
       ),
     );
   }
